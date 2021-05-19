eccbe73eb2cadd5747e692c3616efea970fdd724
radhiant <radhiantsobarna@gmail.com>
2021-05-19 08:47:43 +0700

    update code

--- assets/js/loadcontent.js
+++ assets/js/loadcontent.js
@@ -7,6 +7,16 @@ function loadSMRreport(x) {
     $("#contentMore").load("projek/summaryreport.html");
 }
 
+function loadArsipDok(x) {
+    title.innerHTML = x;
+    $("#contentMore").load("projek/arsipdok.html");
+}
+
+function loadAsetBarang(x) {
+    title.innerHTML = x;
+    $("#contentMore").load("projek/asetbrg.html");
+}
+
 function closeContent() {
     content.innerHTML = "";
 }
