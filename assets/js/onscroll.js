6458152465e91353e8f5767a2bfefbcd8185c488
radhiant <radhiantsobarna@gmail.com>
2021-07-05 00:42:45 +0700

    update code

--- assets/js/onscroll.js
+++ assets/js/onscroll.js
@@ -1,18 +1,18 @@
-$(document).ready(function () { $('#btnup').fadeOut(); });
-window.onscroll = function () { scrollFunction() };
+$(document).ready(function () {
+  $("#btnup").fadeOut();
+});
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-    var navbar = document.getElementById("navbar");
-    var btnup = $('#btnup');
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        navbar.classList.add("shadow");
-        btnup.fadeIn("slow", "linear");
-    } else {
-        navbar.classList.remove("shadow");
-        btnup.fadeOut("slow", "linear");
-    }
-
+  var navbar = document.getElementById("navbar");
+  var btnup = $("#btnup");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    navbar.classList.add("shadow-sm");
+    btnup.fadeIn("slow", "linear");
+  } else {
+    navbar.classList.remove("shadow-sm");
+    btnup.fadeOut("slow", "linear");
+  }
 }
-
-
-
